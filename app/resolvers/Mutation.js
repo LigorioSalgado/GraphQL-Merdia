cb14da77dd89001655d4908ee717dd8b5c5306a4
Edwin Salgado <edwin@devf.mx>
2019-07-01 21:01:38 -0500

    Changes for crud

--- app/resolvers/Mutation.js
+++ app/resolvers/Mutation.js
@@ -1,5 +1,7 @@
 const AuthorModel =  require('../models/Author');
+const PostModel = require('../models/Post');
 const authenticate =  require('../utils/authenticate');
+
 
 const createAuthor =  async(root,params,context,info) => {
 
@@ -17,7 +19,71 @@ const login =  async(root,params,context,info) => {
 	}
 }
 
+const updateProfile = async(root,params,context,info) => {
+
+	const {data} = params
+	const {user} =  context
+	let Author = await AuthorModel.findById(user._id)
+	if(!Author) throw new Error(" Autor No Existe")
+	Author = {...Author,...data}
+	const updatedAuthor = await Author.save({new:true})
+	return updatedAuthor.toObject();
+
+}
+
+const deleteProfile =  async(root,params,context,info) => {
+
+	const {user} =  context;
+	const author = await AuthorModel.findById(user._id);
+	if(!author) throw new Error(" Autor No Existe")
+	author.is_active = false;
+	await author.save({new:true})
+
+	return "Usuario eliminado"
+}
+
+const createPost = async(root,params,context,info) =>{
+
+	const {user} = context;
+	params.data.author = user._id
+	const post = await PostModel.create(params.data)
+								.catch( e => {throw new Error("Error al crear post")} )
+	const newPost = PostModel.findById(post._id).populate('author');
+	await AuthorModel.findOneAndUpdate({_id:user._id},{$push:{posts:post._id}})
+						
+	return newPost;
+}
+
+const updatePost = async(root,params,context,info) => {
+
+	const {id,data} = params;
+	const {user} = context;
+
+	const updatedPost = await PostModel.findOneAndUpdate({_id:id,author:user._id},{$set:{...data}},{new:true})
+
+	return updatedPost.toObject();
+
+}
+
+const deletePost = async(root,params,context,info) => {
+
+	const {id} = params;
+	const {user} = context;
+
+	await PostModel.findOneAndUpdate({_id:id,author:user._id},{$set:{is_active:false}})
+
+	return "Post eliminado"
+
+}
+
+
+
 module.exports = {
 	createAuthor,
-	login
+	login,
+	updateProfile,
+	deleteProfile,
+	createPost,
+	updatePost,
+	deletePost
 }
