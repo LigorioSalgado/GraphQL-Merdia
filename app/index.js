6c79f075bc1aa77a0994e1f125db8c3c84a6f001
Edwin Salgado <edwin@devf.mx>
2019-07-03 20:50:25 -0500

    Author Posts

--- app/index.js
+++ app/index.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
 const { GraphQLServer } =  require('graphql-yoga');
 const { importSchema } = require('graphql-import');
+const { makeExecutableSchema } = require('graphql-tools')
 const mongoose =  require('mongoose');
 const resolvers =  require('./resolvers');
 const typeDefs = importSchema('./app/schema.graphql'); 
 const { AuthDirective } = require('./resolvers/directives');
 const verifyToken =  require('./utils/verifyToken');
 
-mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true })
+
+const MONGO_URI =  process.env.NODE_ENV == 'test' ? process.env.MONGO_URI_TEST 
+												: process.env.MONGO_URI_DEV 
+
+mongoose.connect(MONGO_URI,{ useNewUrlParser: true })
 
 const mongo =  mongoose.connection;
 
@@ -21,17 +26,20 @@ mongo.on('error', (error) => console.log(error))
 	}
 } */
 
-const server =  new GraphQLServer({
+const schema =  makeExecutableSchema({
 	typeDefs,
 	resolvers,
 	schemaDirectives:{
 		auth:AuthDirective
-	},
-	context: async({request}) => verifyToken(request) 
+	}
+})
 
+const server =  new GraphQLServer({
+	schema,
+	context: async({request}) => verifyToken(request) 
 })
 
 server.start(() => console.log("Server is working in port 4000"));
 
 
-
+module.exports = { schema };
